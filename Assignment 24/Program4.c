b854c40198d703181f49e49a58be5ba27fe14f12
Pratibha Sagar Pawar <pratibhapowar1@gmail.com>
2025-11-18 23:33:32 +0530

    Assignment_24 Solution(Q1-Q5)

---
+++ Assignment 24/Program4.c
@@ -0,0 +1,48 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+void Digits(int Arr[], int iLength)
+{
+    int iCnt = 0;
+
+    for(iCnt = 0; iCnt <iLength; iCnt++)
+    {
+        if(Arr[iCnt] >= 100 && Arr[iCnt]<999)
+        {
+            printf("%d\n",Arr[iCnt]);
+        }
+    }
+}
+
+int main()
+{
+    int iSize = 0,iCnt = 0;
+    int *p = NULL;
+
+    printf("Enter number of elements:");
+    scanf("%d",&iSize);
+
+    p = (int*)malloc(iSize * sizeof(int));
+
+    if(p == NULL)
+    {
+        printf("unable to allocate memmry");
+        return -1;
+    }
+
+    printf("Enter %d elements :\n ",iSize);
+
+    for(iCnt = 0; iCnt<iSize; iCnt++)
+    {
+        printf("Enter elements : %d\n",iCnt+1);
+        scanf("%d",&p[iCnt]);
+    }
+
+    Digits( p, iSize);
+
+    free(p);
+
+    return 0;
+
+    
+}
---
+++ Assignment 24/program5.c
@@ -0,0 +1,59 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+void DigitsSum(int Arr[], int iLength)
+{
+    int iCnt = 0, iNo = 0, iDigit = 0;
+    int iSum = 0;
+
+    for(iCnt = 0; iCnt <iLength; iCnt++)
+    {
+       iNo = Arr[iCnt];
+       iSum = 0;
+
+       if(iNo < 0)
+       {
+        iNo = -iNo;
+       }
+       while(iNo != 0)
+       {
+        iDigit = iNo % 10;
+        iSum = iSum + iDigit;
+        iNo= iNo/10;
+       }
+       printf("%d\n",iSum);
+    }
+}
+
+int main()
+{
+    int iSize = 0,iCnt = 0;
+    int *p = NULL;
+
+    printf("Enter number of elements:");
+    scanf("%d",&iSize);
+
+    p = (int*)malloc(iSize * sizeof(int));
+
+    if(p == NULL)
+    {
+        printf("unable to allocate memmry");
+        return -1;
+    }
+
+    printf("Enter %d elements :\n ",iSize);
+
+    for(iCnt = 0; iCnt<iSize; iCnt++)
+    {
+        printf("Enter elements : %d\n",iCnt+1);
+        scanf("%d",&p[iCnt]);
+    }
+
+    DigitsSum( p, iSize);
+
+    free(p);
+
+    return 0;
+
+    
+}
