9a842ecaf4f98a55f8004c4dc43bc374e5ab763b
Pratibha Sagar Pawar <pratibhapowar1@gmail.com>
2025-11-21 14:15:09 +0530

    Assignment_27 Solutions(Q1-Q5)

---
+++ Assignment 27/program2.java
@@ -0,0 +1,74 @@
+////////////////////////////////////////////////////////////////////////////////
+//
+//  Required Package
+//
+////////////////////////////////////////////////////////////////////////////////
+
+import java.util.*;
+
+////////////////////////////////////////////////////////////////////////////////
+//
+//  Class Name    : class pattern
+//  Description   : It is used to Display pattern 
+//  Input         : Intiger  
+//  OutPut        : Pattern 
+//  Author        : Pratibha Sagar Pawar
+//  Date          : 21 Nov 2025
+//      
+////////////////////////////////////////////////////////////////////////////////
+class pattern
+{
+    public void Display(int iRow, int iCol)
+    {
+        int i = 0;
+        int j = 0;
+
+        for(i = 1; i<=iRow; i++)
+        {
+            for(j=1; j<=iCol; j++)
+            {
+                System.out.print(j+"\t");
+            }
+            System.out.println();
+        }
+         System.out.println();    
+    
+    }
+    
+}
+///////////////////////////////////////////////////////////////////////////////
+//
+//  Entery point method for the application 
+// 
+////////////////////////////////////////////////////////////////////////////////
+class program2
+{
+    public static void main(String A[])
+    {
+        Scanner sobj = new Scanner(System.in);
+
+        int iValue1 = 0;
+        int iValue2 = 0;
+
+        System.out.println("Enter the  number of Row : ");
+        iValue1 = sobj.nextInt();
+
+        System.out.println("Enter the number of Column : ");
+        iValue2 = sobj.nextInt();
+
+        pattern pobj = new pattern();
+        pobj.Display(iValue1,iValue2);
+    }
+}
+//  End of main method
+////////////////////////////////////////////////////////////////////////////////
+//
+//  Input  : iRow = 4   iCol = 3
+//
+//  Output : 1   2   3   
+//           1   2   3   
+//           1   2   3   
+//           1   2   3
+//
+////////////////////////////////////////////////////////////////////////////////
+
