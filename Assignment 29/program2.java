6ea72509529717d1689f98981b058f9521ea4bc7
Pratibha Sagar Pawar <pratibhapowar1@gmail.com>
2025-11-25 00:32:43 +0530

    Assignment_29 Solution(Q1-Q5)

---
+++ Assignment 29/program2.java
@@ -0,0 +1,89 @@
+////////////////////////////////////////////////////////////////////////////////
+//
+//  Required Package
+//
+////////////////////////////////////////////////////////////////////////////////
+
+
+import java.util.Scanner;
+
+////////////////////////////////////////////////////////////////////////////////
+//
+//  Class Name    : class pattern
+//  Description   : It is used to Display pattern 
+//  Input         : Intiger  
+//  OutPut        : Pattern 
+//  Author        : Pratibha Sagar Pawar
+//  Date          : 22 Nov 2025
+//      
+////////////////////////////////////////////////////////////////////////////////
+class pattern
+{
+    public void Display(int iRow, int iCol)
+    {
+        int i = 0;
+        int j = 0;
+
+        for(i = 1; i<=iRow; i++ )
+        {
+            for(j = 1; j<=iCol; j++)
+            {
+                if(i % 2 == 1)
+                {
+                    System.out.print((j*2)+"\t"); 
+                    
+      
+                }
+                else
+                {
+                 System.out.print(j+"\t");
+                    
+                }
+                
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+
+}
+
+///////////////////////////////////////////////////////////////////////////////
+//
+//  Entery point method for the application 
+// 
+////////////////////////////////////////////////////////////////////////////////
+
+class program2
+{
+    public static void main(String A[])
+    {
+        Scanner sobj = new Scanner(System.in);
+
+        int iValue1 = 0;
+        int iValue2 = 0;
+
+        System.out.println("Enter the number of Rows : ");
+        iValue1 = sobj.nextInt();
+
+        System.out.println("Enter the number of columns : ");
+        iValue2 = sobj.nextInt();
+
+        pattern pobj = new pattern();
+        pobj.Display(iValue1, iValue2);
+
+    }
+}
+//  End of main method
+////////////////////////////////////////////////////////////////////////////////
+//
+//  Input  : iRow = 4  iCol = 4
+//
+//  Output : 2   4   6   8
+//           1   2   3   4
+//           2   4   6   8
+//           1   2   3   4
+//
+////////////////////////////////////////////////////////////////////////////////
+
+
---
+++ Assignment 29/program3.java
@@ -0,0 +1,85 @@
+////////////////////////////////////////////////////////////////////////////////
+//
+//  Required Package
+//
+////////////////////////////////////////////////////////////////////////////////
+
+import java.util.Scanner;
+
+////////////////////////////////////////////////////////////////////////////////
+//
+//  Class Name    : class pattern
+//  Description   : It is used to Display pattern 
+//  Input         : Intiger  
+//  OutPut        : Pattern 
+//  Author        : Pratibha Sagar Pawar
+//  Date          : 22 Nov 2025
+//      
+////////////////////////////////////////////////////////////////////////////////
+class pattern
+{
+    public void Display(int iRow, int iCol)
+    {
+        int i = 0;
+        int j = 0;
+        char ch = '\0';
+
+       
+            for(i = 1; i<=iRow; i++ )
+        {
+            for(j = 1, ch = 'a'; j<=iCol; j++, ch++)
+            {
+                if(i % 2 == 0)
+                {
+                    System.out.printf("%d\t",j);
+                }
+                else
+                {
+                 System.out.printf("%c\t",ch);
+                }
+                
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+}
+///////////////////////////////////////////////////////////////////////////////
+//
+//  Entery point method for the application 
+// 
+////////////////////////////////////////////////////////////////////////////////
+
+class program3
+{
+    public static void main(String A[])
+    {
+        Scanner sobj = new Scanner(System.in);
+
+        int iValue1 = 0;
+        int iValue2 = 0;
+
+        System.out.println("Enter the number of Rows : ");
+        iValue1 = sobj.nextInt();
+
+        System.out.println("Enter the number of columns : ");
+        iValue2 = sobj.nextInt();
+
+        pattern pobj = new pattern();
+        pobj.Display(iValue1, iValue2);
+
+    }
+}
+//  End of main method
+////////////////////////////////////////////////////////////////////////////////
+//
+//  Input  : iRow = 4  iCol = 4
+//
+//  Output : a   b   c   d
+//           1   2   3   4
+//           a   b   c   d
+//           1   2   3   4
+//
+////////////////////////////////////////////////////////////////////////////////
+
+
