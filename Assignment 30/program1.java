811c334cc5013e9c00c7b6b200f42816bc5af65a
Pratibha Sagar Pawar <pratibhapowar1@gmail.com>
2025-11-25 00:33:32 +0530

    Assignment_30 Solution(Q1-Q5)

---
+++ Assignment 30/program1.java
@@ -0,0 +1,78 @@
+////////////////////////////////////////////////////////////////////////////////
+
+import java.util.Scanner;
+
+////////////////////////////////////////////////////////////////////////////////
+//
+//  Class Name    : class pattern
+//  Description   : It is used to Display pattern 
+//  Input         : Intiger  
+//  OutPut        : Pattern 
+//  Author        : Pratibha Sagar Pawar
+//  Date          : 22 Nov 2025
+//      
+////////////////////////////////////////////////////////////////////////////////
+class pattern
+{
+    public void Display(int iRow, int iCol)
+    {
+        int i = 0;
+        int j = 0;
+       
+            for(i = 1; i<=iRow; i++ )
+        {
+            for(j = 1; j<=iCol; j++)
+            {
+                if(i>=j)
+                {
+                    System.out.print("*\t");
+                }
+                else
+                {
+                   System.out.print("#\t");
+                }
+                
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+}
+///////////////////////////////////////////////////////////////////////////////
+//
+//  Entery point method for the application 
+// 
+////////////////////////////////////////////////////////////////////////////////
+
+class program1
+{
+    public static void main(String A[])
+    {
+        Scanner sobj = new Scanner(System.in);
+
+        int iValue1 = 0;
+        int iValue2 = 0;
+
+        System.out.println("Enter the number of Rows : ");
+        iValue1 = sobj.nextInt();
+
+        System.out.println("Enter the number of columns : ");
+        iValue2 = sobj.nextInt();
+
+        pattern pobj = new pattern();
+        pobj.Display(iValue1, iValue2);
+
+    }
+}
+//  End of main method
+////////////////////////////////////////////////////////////////////////////////
+//
+//  Input  : iRow = 4  iCol = 4
+//
+//  Output :  *    #    #    #
+//            *    *    #    #
+//            *    *    *    #
+//            *    *    *    *
+////////////////////////////////////////////////////////////////////////////////
+
+
