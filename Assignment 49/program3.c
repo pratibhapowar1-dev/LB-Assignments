6b70eba91323046af0d6e280950f58d3fc3626ef
Pratibha Sagar Pawar <pratibhapowar1@gmail.com>
2026-01-16 11:51:47 +0530

    Assignment_49Solution(Q1-Q5)

---
+++ Assignment 49/program3.c
@@ -0,0 +1,97 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+# pragma pack(1)
+struct node
+{
+    int data;
+    struct node *next;
+};
+
+typedef struct node NODE;
+typedef struct node * PNODE;
+typedef struct node ** PPNODE;
+
+void InsertFirst(PNODE *first, int no)
+{
+    PNODE newn = NULL;
+    newn = (PNODE)malloc(sizeof(NODE));
+
+    newn -> data = no;
+    newn -> next = NULL;
+
+    if(*first == NULL)
+    {
+        *first = newn;
+    }
+    else
+    {
+        newn -> next = *first;
+        *first = newn;
+    }
+
+}
+void Display(PNODE first)
+{
+    while(first != NULL)
+    {
+        printf(" | %d |->", first ->data);
+        first = first -> next;
+    }
+    printf("NULL\n");
+
+}
+int Count(PNODE first)
+{
+    int iCount = 0;
+    while (first != NULL)
+    {
+        iCount++;
+        first = first -> next;
+    }
+    return iCount;  
+
+}
+void DisplayDivideByThree(PNODE first)
+{
+
+    printf("Elements divide by 3 is : ");
+    while (first != NULL)
+    {
+        if(first -> data % 3 == 0 )
+       {
+         
+          printf("%d\t",first -> data);
+
+       }
+       first= first -> next;
+
+    }
+    printf("\n");
+    
+ 
+}
+
+int main()
+{
+    PNODE head = NULL;
+    int iRet = 0;
+    int bRet = 0;
+   
+    InsertFirst(&head,75);
+    InsertFirst(&head,54);
+    InsertFirst(&head,47);
+    InsertFirst(&head,36);
+    InsertFirst(&head,24);
+    InsertFirst(&head,11);
+
+    printf("Original linked list is : \n");
+    Display(head);
+    iRet = Count(head);
+    printf("Number of nodes are : %d\n",iRet);
+
+    DisplayDivideByThree(head);
+   
+
+    return 0;
+}
---
+++ Assignment 49/program5.c
@@ -0,0 +1,96 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+# pragma pack(1)
+struct node
+{
+    int data;
+    struct node *next;
+};
+
+typedef struct node NODE;
+typedef struct node * PNODE;
+typedef struct node ** PPNODE;
+
+void InsertFirst(PNODE *first, int no)
+{
+    PNODE newn = NULL;
+    newn = (PNODE)malloc(sizeof(NODE));
+
+    newn -> data = no;
+    newn -> next = NULL;
+
+    if(*first == NULL)
+    {
+        *first = newn;
+    }
+    else
+    {
+        newn -> next = *first;
+        *first = newn;
+    }
+
+}
+void Display(PNODE first)
+{
+    while(first != NULL)
+    {
+        printf(" | %d |->", first ->data);
+        first = first -> next;
+    }
+    printf("NULL\n");
+
+}
+int Count(PNODE first)
+{
+    int iCount = 0;
+    while (first != NULL)
+    {
+        iCount++;
+        first = first -> next;
+    }
+    return iCount;  
+
+}
+int CountTwoDigit(PNODE first)
+{
+    int iCount = 0;
+    int no = 0;
+
+    while (first != NULL)
+    {
+        no = first -> data;
+        if((no >= 10 && no <= 99)|| (no >= -10 && no <-99))
+        {
+            iCount++;
+        }
+        first = first -> next;
+    }
+    return iCount;
+    
+ 
+}
+
+int main()
+{
+    PNODE head = NULL;
+    int iRet = 0;
+    int bRet = 0;
+   
+    InsertFirst(&head,745);
+    InsertFirst(&head,54);
+    InsertFirst(&head,47);
+    InsertFirst(&head,36);
+    InsertFirst(&head,24);
+    InsertFirst(&head,1);
+
+    printf("Original linked list is : \n");
+    Display(head);
+    iRet = Count(head);
+    printf("Number of nodes are : %d\n",iRet);
+
+   bRet = CountTwoDigit(head);
+   printf("Count of two digits number  : %d\n ", bRet);
+
+    return 0;
+}
